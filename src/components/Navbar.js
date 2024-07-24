3aee61df973715f46715ee7e143770649050af40
Aniket Bharane <codeaniket123@gmail.com>
2024-07-24 21:47:43 +0530

    small change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,7 +17,7 @@ function Navbar() {
       <a className="mr-5 hover:text-yellow-600" href='/'>Third Link</a>
       <a className="mr-5 hover:text-yellow-600" href='/'>Fourth Link</a>
     </nav>
-    <button className="inline-flex text-white items-center bg-dr border-0 py-1 px-3 focus:outline-none hover:bg-yellow-600 rounded text-base mt-4 md:mt-0">Button
+    <button className="inline-flex text-white items-center bg-dr border-0 py-1 px-3 focus:outline-none hover:bg-yellow-600 rounded text-base mt-4 md:mt-0">Login
       <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
         <path d="M5 12h14M12 5l7 7-7 7"></path>
       </svg>
